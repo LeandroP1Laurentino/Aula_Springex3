a84815a69aac33204bed7020fb77a99b51ef6053
LEANDRO PESSOA LAURENTINO <leandro.plaurentino@senacsp.edu.br>
2026-09-15 10:07:58 -0300

    validaçoes

--- src/main/java/br/senac/tads/dsw/exemplo/model/Produto.java
+++ src/main/java/br/senac/tads/dsw/exemplo/model/Produto.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 @Entity
 public class Produto {
@@ -12,9 +15,13 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank 
     private String nome;
-    private Double preco;
     
+    @NotNull 
+    @Positive
+    private Double preco;
+     
     public Produto() {
     }
 
